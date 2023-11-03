3c03da9e601008d573647606f48798fbca318453
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-11-03 08:51:27 +0700

    commit

--- app/src/main/java/me/ababil/registerloginapp/MainActivity.kt
+++ app/src/main/java/me/ababil/registerloginapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package me.ababil.registerloginapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var etName: EditText
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
+    private lateinit var tvNewActivity: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,16 @@ class MainActivity : AppCompatActivity() {
         etName = findViewById(R.id.et_name)
         etEmail = findViewById(R.id.et_email)
         etPassword = findViewById(R.id.et_password)
+        tvNewActivity = findViewById(R.id.tv_new_activity)
+
+        tvNewActivity.text = getString(R.string.you_have_an_account_sign_here)
+        tvNewActivity.setTextColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
+        tvNewActivity.isClickable = true
+        tvNewActivity.isFocusable = true
+        tvNewActivity.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         val btnRegister: Button = findViewById(R.id.btn_register)
         btnRegister.setOnClickListener {
